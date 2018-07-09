50828601d3e755150051016e1ac46c71694bafd7
BLaZeKiLL <laldevashish@gmail.com>
2018-07-09 18:08:27 +0530

    PlayerPrefs interface created

---
+++ GardenWarfare/Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPrefsManager : MonoBehaviour
+{
+    const string MASTER_VOLUME_KEY = "master_volume";
+    const string DIFFICULTY_KEY = "difficulty";
+    const string LEVEL_KEY = "level_unlocked_";
+
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume > 0f && volume < 1f)
+        {
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        }
+        else
+        {
+            Debug.LogError("Master Volume out of range");
+        }
+    }
+
+    public static float GetMasTerVolume()
+    {
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+
+    public static void UnlockLevel(int level)
+    {
+        if (level <= SceneManager.sceneCountInBuildSettings -1)
+        {
+            PlayerPrefs.SetInt(LEVEL_KEY + level.ToString(), 1);
+        }
+        else
+        {
+            Debug.LogError("Trying To unlock a level not in build order");
+        }
+    }
+    
+    public static bool IsLevelUnlocked(int level)
+    {
+        if (level <= SceneManager.sceneCountInBuildSettings - 1)
+        {
+            return (PlayerPrefs.GetInt(LEVEL_KEY + level.ToString()) == 1) ? true : false;
+        }
+        else
+        {
+            Debug.LogError("Trying To query a level not in build order");
+            return false;
+        }
+    }
+
+    public static void SetDifficulty(float difficulty)
+    {
+        if (difficulty > 0f && difficulty < 1f)
+        {
+            PlayerPrefs.SetFloat(DIFFICULTY_KEY, difficulty);
+        }
+        else
+        {
+            Debug.LogError("Difficulty out of range");
+        }
+    }
+
+    public static float GetDifficulty()
+    {
+        return PlayerPrefs.GetFloat(DIFFICULTY_KEY);
+    }
+}
